a519deb5d481b26d571eab05b324a449809c05a7
Junejune95 <51689902+Junejune95@users.noreply.github.com>
2022-12-10 23:09:57 +0530

    3 files modified

--- src/app/form-builder/form-builder.component.ts
+++ src/app/form-builder/form-builder.component.ts
@@ -1,8 +1,22 @@
 import { Component } from '@angular/core';
 
+import { FormBuilder } from '@angular/forms';
+
 @Component({
   selector: 'app-form-builder',
-  template: './form-builder.component.html',
+  templateUrl: './form-builder.component.html',
   styleUrls: ['./form-builder.component.css'],
 })
-export class FormBuilderComponent {}
+export class FormBuilderComponent {
+  myform = this.fb.group({
+    firstName: [''],
+    lastName: [''],
+    about: [''],
+    contact: this.fb.group({
+      email: [''],
+      phoneNumber: [''],
+      address: [''],
+    }),
+  });
+  constructor(private fb: FormBuilder) {}
+}
