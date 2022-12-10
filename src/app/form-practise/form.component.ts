8255f27b5080d489d0b115d987c68744ecbfe96c
Junejune95 <51689902+Junejune95@users.noreply.github.com>
2022-12-10 11:10:47 +0530

    simple form create

---
+++ src/app/form-practise/form.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-form-practise',
+  templateUrl: './form.component.html',
+  styleUrls: ['./form.component.css'],
+})
+export class FormComponent {}
