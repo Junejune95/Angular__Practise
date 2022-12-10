407fb9c23326033b219d7804bddcdee604afe1c9
Junejune95 <51689902+Junejune95@users.noreply.github.com>
2022-12-10 20:24:57 +0530

    add form builder componet

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import { FormComponent } from './form-practise/form.component';
+import { FormBuilderComponent } from './form-builder/form-builder.component';
 
 @NgModule({
-  declarations: [AppComponent, FormComponent],
+  declarations: [AppComponent, FormComponent, FormBuilderComponent],
   imports: [BrowserModule, AppRoutingModule, ReactiveFormsModule],
   providers: [],
   bootstrap: [AppComponent],
---
+++ src/app/form-builder/form-builder.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-form-builder',
+  template: './form-builder.component.html',
+  styleUrls: ['./form-builder.component.css'],
+})
+export class FormBuilderComponent {}
