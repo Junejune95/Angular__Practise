dff194bfca2a29d987c1600103ee9d9ace7fa111
Junejune95 <51689902+Junejune95@users.noreply.github.com>
2022-12-10 14:38:19 +0530

    create two form with form control name and form control

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
@@ -8,7 +9,7 @@ import { FormComponent } from './form-practise/form.component';
 
 @NgModule({
   declarations: [AppComponent, FormComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, ReactiveFormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/form-practise/form.component.ts
+++ src/app/form-practise/form.component.ts
@@ -1,8 +1,27 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-form-practise',
   templateUrl: './form.component.html',
   styleUrls: ['./form.component.css'],
 })
-export class FormComponent {}
+export class FormComponent {
+  userForm = new FormGroup({
+    title: new FormControl(''),
+    firstName: new FormControl(''),
+    lastName: new FormControl(''),
+    acceptTerm: new FormControl(''),
+  });
+
+  get f() {
+    return this.userForm.controls;
+  }
+
+  onSubmit() {
+    const result = this.userForm.value;
+    console.log('Title:' + result.title);
+    console.log('First Name:' + result.firstName);
+    console.log('Last Name:' + result.lastName);
+  }
+}
