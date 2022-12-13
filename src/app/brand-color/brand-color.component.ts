84aacf6c909dc1f42847bf930f610a073539ddd2
Junejune95 <51689902+Junejune95@users.noreply.github.com>
2022-12-13 11:03:49 +0630

    2 files modified

--- src/app/brand-color/brand-color.component.ts
+++ src/app/brand-color/brand-color.component.ts
@@ -1,12 +1,23 @@
-import { Component, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
+import {
+  Component,
+  ViewChild,
+  ElementRef,
+  AfterViewInit,
+  OnInit,
+} from '@angular/core';
 
 @Component({
   selector: 'app-brand-color',
   templateUrl: './brand-color.component.html',
   styleUrls: ['./brand-color.component.css'],
 })
-export class BrandColorComponent implements AfterViewInit {
+export class BrandColorComponent implements OnInit, AfterViewInit {
   @ViewChild('someElement', { static: false }) someElement: ElementRef;
+
+  ngOnInit() {
+    console.log(this.someElement);
+    this.someElement.nativeElement.innerHTML = 'Hello Inner HTML';
+  }
 
   ngAfterViewInit() {
     console.log(this.someElement);
