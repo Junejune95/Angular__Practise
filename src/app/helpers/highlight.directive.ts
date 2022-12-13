fe7a37b654361572bac7f341c43ef48c051d9523
Junejune95 <51689902+Junejune95@users.noreply.github.com>
2022-12-13 23:35:40 +0630

    2 files modified

--- src/app/helpers/highlight.directive.ts
+++ src/app/helpers/highlight.directive.ts
@@ -1,11 +1,29 @@
-import { Directive, HostBinding, HostListener } from '@angular/core';
+import { Directive, HostListener, ElementRef, Input } from '@angular/core';
 
 @Directive({
   selector: '[appHighlight]',
 })
 export class HighlightDirective {
+  @Input() defaultColor: string;
+
+  constructor(private el: ElementRef) {
+    console.log(this.defaultColor);
+    this.onHighLight(this.defaultColor);
+  }
+
   @HostListener('mouseenter')
   onMouseEnter() {
     console.log('enter');
+    this.onHighLight('pink');
+  }
+
+  @HostListener('mouseleave')
+  onMouseLeave() {
+    console.log('leave');
+    this.onHighLight('orange');
+  }
+
+  onHighLight(color: string) {
+    this.el.nativeElement.style.color = color;
   }
 }
