ae1155adf70d4098197998107a81119cd10a74e9
Junejune95 <51689902+Junejune95@users.noreply.github.com>
2022-12-13 10:46:19 +0630

    2 files modified

--- src/app/brand-color/brand-color.component.ts
+++ src/app/brand-color/brand-color.component.ts
@@ -1,8 +1,12 @@
-import { Component } from '@angular/core';
+import { Component,ViewChild,ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-brand-color',
   templateUrl: './brand-color.component.ts',
   styleUrls: ['./brand-color.component.css'],
 })
-export class BrandColorComponent {}
+export class BrandColorComponent {
+  @ViewChild('herehtml') ele:ElementRef;
+
+  
+}
