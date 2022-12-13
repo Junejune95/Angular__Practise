8ccaf01eb2349eff6a5277972c9cda8b5073a03d
Junejune95 <51689902+Junejune95@users.noreply.github.com>
2022-12-13 17:09:03 +0630

    4 files modified

--- src/app/brand-color/brand-color.component.ts
+++ src/app/brand-color/brand-color.component.ts
@@ -6,8 +6,9 @@ import {
   OnInit,
   ViewChildren,
   QueryList,
+  Input,
 } from '@angular/core';
-// import { NgModel } from '@angular/forms';
+import { NgModel } from '@angular/forms';
 
 @Component({
   selector: 'app-brand-color',
@@ -15,21 +16,27 @@ import {
   styleUrls: ['./brand-color.component.css'],
 })
 export class BrandColorComponent implements OnInit, AfterViewInit {
+  @Input()
+  text: any;
+
   @ViewChild('someElement', { static: false }) elementDynamic: ElementRef;
   @ViewChild('someElement2', { static: true }) elementStatic: ElementRef;
 
-  // @ViewChildren('NgModel') domReference: QueryList<NgModel>;
+  @ViewChildren('applicationInfo') domReference: QueryList<String>;
   public userName: any;
   public userAge: any;
   public userDesignation: any;
 
   ngOnInit() {
     console.log(this.elementStatic);
+    console.log(this.domReference);
     this.elementStatic.nativeElement.innerHTML = 'Hello Static';
   }
 
   ngAfterViewInit() {
+    console.log(this.domReference);
     console.log(this.elementDynamic);
     this.elementDynamic.nativeElement.innerHTML = 'Hello Dynamic';
+    this.elementDynamic.nativeElement.style = 'color:red';
   }
 }
--- src/app/propertyDecorator/propertydeco.component.ts
+++ src/app/propertyDecorator/propertydeco.component.ts
@@ -1,14 +1,28 @@
-import { Component } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  QueryList,
+  ViewChild,
+  ViewChildren,
+} from '@angular/core';
+import { NgModel } from '@angular/forms';
+import { BrandColorComponent } from '../brand-color/brand-color.component';
 
 @Component({
   selector: 'app-property-deco',
   templateUrl: './propertydeco.component.html',
   styleUrls: ['./propertydeco.component.css'],
 })
-export class PropertyDecoComponent {
-  childtitle="You are my child";
+export class PropertyDecoComponent implements AfterViewInit {
+  childtitle = 'You are my child';
 
-  onSayHello(e:any){
+  @ViewChildren('BrandColorComponent') bcom: QueryList<NgModel>;
+
+  onSayHello(e: any) {
     alert(e);
+  }
+
+  ngAfterViewInit() {
+    console.log(this.bcom.length,'Here Property DEco');
   }
 }
