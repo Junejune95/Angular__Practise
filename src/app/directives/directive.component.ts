10b8e4c42211776ca84eb58aa4aee60754d87e3e
Junejune95 <51689902+Junejune95@users.noreply.github.com>
2022-12-14 16:41:49 +0630

    2 files modified

--- src/app/directives/directive.component.ts
+++ src/app/directives/directive.component.ts
@@ -5,4 +5,33 @@ import { Component } from '@angular/core';
   templateUrl: './directive.component.html',
   styleUrls: ['./directive.component.css'],
 })
-export class DirectiveComponent {}
+export class DirectiveComponent {
+  public isTemplate2 = true;
+  people: any[] = [
+    {
+      name: 'Douglas  Pace',
+      age: 35,
+      country: 'UK',
+    },
+    {
+      name: 'Mcleod  Mueller',
+      age: 32,
+      country: 'UK',
+    },
+    {
+      name: 'Day  Meyers',
+      age: 21,
+      country: 'US',
+    },
+    {
+      name: 'Aguirre  Ellis',
+      age: 34,
+      country: 'MM',
+    },
+    {
+      name: 'Cook  Tyson',
+      age: 32,
+      country: 'TH',
+    },
+  ];
+}
